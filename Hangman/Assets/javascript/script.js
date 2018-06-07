4624cc1d343f93f8c36da0050059d6e1ac620ca5
ColeAmodeo <camodeo1@live.ca>
2018-06-07 16:00:43 -0400

    basic hangman function

--- Hangman/Assets/javascript/script.js
+++ Hangman/Assets/javascript/script.js
@@ -1,43 +1,89 @@
 
 
 var wordBank = [
-    'Apple', 'Banana', 'Pineapple', 'Grapefruit', 'Kiwi', 'Pear',
-    'Watermelon', 'Dragonfruit', 
-];
-var activeWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-var wordArray = activeWord.split("")
+    'apple', 'banana', 'pineapple', 'grapefruit', 'kiwi', 'pear',
+    'watermelon', 'dragonfruit', ];
+var word;
+var wordField = [];
+var usedLetters = [];
+var wins = 0;
+var losses = 0;
 var guesses = 5;
-var wordField = [ ];
-var win = document.getElementById('wins');
-var lose = document.getElementById('losses');
+var blanks;
+var index;
 
-
-
-function startGame() {
-    
-    createWordField();
-    guessCheck();
-    
+var randomWord = function() {
+index = Math.floor(Math.random() * wordBank.length);
+word = wordBank[index];
+blanks = word.length;
 }
-function createWordField () {
-    for (i = 0; i < activeWord.length; i++){
-    wordField.push('_');
-    }
-    
-    document.getElementById('word-field').innerHTML = wordField.join(" ")
 
+var generateBlanks = function() {
+   for (i = 0; i < blanks; i++) {
+       wordField.push("_");
+   }
+   document.getElementById("word").textContent = wordField.join(" ");
 }
 
 
-//input
+var startGame = function() {
+   guesses = 5;
+   wordField = [];
+   usedLetters = [];
+   randomWord();
+   generateBlanks();
+   document.getElementById("letters").textContent = usedLetters.join(" ").toUpperCase();
+   document.getElementById("guesses").textContent = guesses;
+}
+
+startGame();
+
+var checkLetter = function(letter) {
+   var regEx = /[a-z]/;
+   if (regEx.test(letter)) {
+       wordArr = word.split("");
+       var checkedIndex = wordArr.indexOf(letter);
+
+       if (checkedIndex < 0 && usedLetters.indexOf(letter) < 0) {
+           usedLetters.push(letter);
+           guesses--;
+           }
+
+       while (checkedIndex >= 0) {
+           wordField.splice(checkedIndex, 1, letter);
+           delete wordArr[checkedIndex];
+           checkedIndex = wordArr.indexOf(letter);
+       }
+
+       if (wordField.join('') == word) {
+           wins++;
+           word = "";
+           wordBank.splice(index, 1);
+           setTimeout(function(){ startGame(); }, 1500);
+       }
+
+       if (guesses === 0) {
+           losses++;
+           word = "";
+           wordBank.splice(index, 1);
+           setTimeout(function(){ startGame(); }, 1500);
+       }
+       
+       document.getElementById("word").textContent = wordField.join(" ");
+       document.getElementById("letters").textContent = usedLetters.join(" ").toUpperCase();
+       document.getElementById("wins").textContent = wins;
+       document.getElementById("losses").textContent = losses;
+       document.getElementById("guesses").textContent = guesses;
+
+   }
+
+}
+
 document.onkeyup = function() {
-    var keyPress = event.key;
-    function checkLetter(keypress) {
-        
-
-    }
-
- }
+   var keyPress = event.key;
+   checkLetter(keyPress);
+   
+}
 
 
 function guessCheck(){
@@ -59,19 +105,8 @@ function guessCheck(){
 }
 
 
-startGame();
 
 
-//function buttonClick(){  TEST TO SEE IF SNAKE WORKS! success
-   
-       // guesses--;
-      //  guessCheck();
-        //console.log(guesses)
-
-
-
-//Using the input, check if the letter matches the wordBank array
-// if it does, fill the keypress into the wordField array
 
 
 
